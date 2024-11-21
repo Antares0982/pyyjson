8eb6676f26d44d704c3fb62319da763d6e1b121e
eritque0arcus <eritque-arcus@ikuyo.dev>
2024-11-21 15:03:40 -0600

    chore: use builtin implementation for fallback

--- src/simd/simd_impl.h
+++ src/simd/simd_impl.h
@@ -463,7 +463,7 @@ force_inline u32 tzcnt_u32(u32 x) {
     return _mm_tzcnt_32(x);
 #endif
 #else
-    return 31 - (((x & -x) & 0x0000FFFF) ? 16 : 0) - (((x & -x) & 0x00FF00FF) ? 8 : 0) - (((x & -x) & 0x0F0F0F0F) ? 4 : 0) - (((x & -x) & 0x33333333) ? 2 : 0) - (((x & -x) & 0x55555555) ? 1 : 0);
+    return _bit_scan_forward(x);
 #endif
 }
 
@@ -478,7 +478,8 @@ force_inline u64 tzcnt_u64(u64 x) {
     return _mm_tzcnt_64(x);
 #endif
 #else
-    return 63 - (((x & -x) & 0x00000000FFFFFFFFULL) ? 32 : 0) - (((x & -x) & 0x0000FFFF0000FFFFULL) ? 16 : 0) - (((x & -x) & 0x00FF00FF00FF00FFULL) ? 8 : 0) - (((x & -x) & 0x0F0F0F0F0F0F0F0FULL) ? 4 : 0) - (((x & -x) & 0x3333333333333333ULL) ? 2 : 0) - (((x & -x) & 0x5555555555555555ULL) ? 1 : 0);
+    // TODO: maybe use _bit_scan_forward64 in WIN32 MSVC
+    return __builtin_ctzll(x);
 #endif
 }
 
